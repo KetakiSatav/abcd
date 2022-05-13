75aa7527e155b437d405b0522f5c73df6dc72aba
Ketaki Satav <ketaki@LAPTOP-CN3U58ER>
2022-05-13 18:02:37 +0530

    First Commit

---
+++ WAD/AJS/file.js
@@ -0,0 +1,6 @@
+Student([
+    {"Name":"AAA", "roll_no":1, "city":"pune"},
+    {"Name":"BBB", "roll_no":2, "city":"bmt"},
+    {"Name":"CCC", "roll_no":3, "city":"Mumbai"},
+    {"Name":"DDD", "roll_no":4, "city":"Banglore"}
+])
